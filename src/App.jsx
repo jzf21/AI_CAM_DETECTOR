c2964bdc3b35bfbd2b5082810dc78e1f1df96bc5
jzf21 <jozefneelamkavil2003@gmail.com>
2024-02-29 21:45:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CameraComponent from "./components/CamDetector";
+
+function App() {
+  return (
+    <>
+      <CameraComponent />
+      <p>hello</p>
+    </>
+  );
+}
+
+export default App;
